f847d31dfcd2f03208ed8be30fa410b74f75bcef
Carlos Raul <carlos.primo@unmsm.edu.pe>
2022-07-12 16:38:19 -0500

    Primer commit

---
+++ INSimpson1_3.f90
@@ -0,0 +1,23 @@
+program integracionconsimpson
+use funcion
+implicit none
+integer n,i
+real*8::a,b,h,rsl,x1,x0,y0,y1,nrm,sumapar, sumaimp
+    write(*,*)'ingrese el intervalo'
+    read(*,*)a,b
+    write(*,*)'Ingrese el valor de la particion par'
+    read(*,*)n
+        h=(b-a)/n !paso de integracion numerica simpson 1/3
+        nrm=h; sumaimp=0;sumapar=0
+    do i=1,n-1,2
+        sumaimp=sumaimp+4*f(a+i*h)
+    end do 
+    do i=2,n-2,2
+        sumapar=sumapar+2*f(a+i*h)
+    end do
+        rsl=(nrm/3)*(f(a)+f(b)+ sumapar +sumaimp)
+    123 format(A,ES15.5,A,ES15.5,A)
+    write(*,*)'La integracion de 3x^2 con el la regla de Simposon 1/3'
+    write(*,123)'Desde ',a,' hasta',b,' es'
+    write(*,*)rsl
+end program integracionconsimpson
---
+++ funcion.f90
@@ -0,0 +1,8 @@
+module funcion 
+contains 
+function f(x)   
+    real*8,intent(in)::x
+        f=3*x**2 
+        return
+end function   
+end module 
